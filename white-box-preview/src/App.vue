41b1e44b7f704993f8797fbfb0c8263a952eccda
邹通成 <chengzou233@gmail.com>
2022-04-10 13:58:23 +0800

    init

---
+++ white-box-preview/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <LoginView></LoginView>
+</template>
+
+<script>
+import LoginView from "@/views/LoginView";
+
+export default {
+  name: 'App',
+  components: {
+    LoginView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ white-box-preview/src/views/LoginView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="centerFrame">
+    <el-image :src="imagePath" />
+    <el-form :inline="true" label-width="auto" label-position="center" style="vertical-align: center">
+      <div class="inputFrame">
+        <el-form-item label="帐号" style="width: 100%">
+          <el-input class="inputArea" size="large" v-model="value" placeholder="帐号"></el-input>
+        </el-form-item>
+      </div>
+      <div class="inputFrame">
+        <el-form-item label="密码" style="width: 100%">
+          <el-input class="inputArea" size="large" placeholder="密码"></el-input>
+        </el-form-item>
+      </div>
+      <div class="inputFrame" style="text-align: left">
+        <el-form-item label="验证码" style="width: 80%">
+          <el-input class="inputArea" size="large" placeholder="验证码"></el-input>
+        </el-form-item>
+        <el-button type="primary" size="large" style="vertical-align: top; width: 10%">发送</el-button>
+      </div>
+      <div class="inputFrame">
+        <el-button type="success" size="large" round style="width: 45%"> 登陆 </el-button>
+        <el-button type="danger" size="large" round style="width: 45%"> 注册 </el-button>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+  data() {
+    return {
+      imagePath : require('../assets/logo.png'),
+      value: ""
+    }
+  },
+  methods : {
+    inputAccount() {
+    }
+  }
+}
+</script>
+
+<style scoped>
+.centerFrame {
+  -webkit-transform: translateY(50%);
+}
+
+.inputArea >>> .el-input__inner{
+  border-radius: 30px;
+  display: inline;
+}
+
+.inputFrame {
+  width: 30%;
+  margin: 0 auto;
+}
+</style>
